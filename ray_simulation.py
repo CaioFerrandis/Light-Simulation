0e61218888d81d2729de7cdac0a93426bd4116b6
Caio Ferrandis <ferrandis.ca@gmail.com>
2024-04-29 23:56:46 -0300

    Added .exe file and updated simulation.

--- ray_simulation.py
+++ ray_simulation.py
@@ -1,3 +1,9 @@
+'''
+Simulation of light rays reflecting off of a concave mirror
+
+C.F.
+'''
+
 import math
 
 import pygame
@@ -14,10 +20,11 @@ running = True
 sub_steps = 8
 
 # Define circle properties
-circle_radius = screen.get_width()
-circle_center = [0, screen.get_height()/2]
+circle_radius = WIDTH
+circle_center = [0, HEIGHT/2]
 circle_border = 5
-focus = circle_radius/2
+center = WIDTH - 150
+focus = WIDTH - 75
 
 while running:
     mouse = pygame.mouse.get_pos()
@@ -27,8 +34,6 @@ while running:
         if e.type == pygame.QUIT:
             running = False
 
-    angular_coefficient = (circle_center[1] - mouse[1]) / (circle_radius - mouse[0])
-
     # Draw center line for reference
     pygame.draw.line(screen, (200, 200, 200), [0, circle_center[1]],
                      [WIDTH, circle_center[1]], 1)
@@ -37,20 +42,25 @@ while running:
     # Draw circular mirror and its components
     pygame.draw.circle(screen, [255, 255, 255], circle_center, circle_radius, circle_border)
     pygame.draw.circle(screen, [255, 255, 255], [focus, circle_center[1]], 3)
-    pygame.draw.circle(screen, [255, 255, 255], [circle_center[0], circle_center[1]], 3)
+    pygame.draw.circle(screen, [255, 255, 255], [center, circle_center[1]], 3)
     pygame.draw.rect(screen, [50, 50, 50], pygame.Rect(circle_center[0]-circle_radius,
                                                        circle_center[1]-circle_radius,
                                                        circle_radius, circle_radius*2))
 
     # Draw predicted particle trajectory
     # Towards focus of mirror
-    pygame.draw.line(screen, (255, 0, 0), (mouse[0], mouse[1]), ((circle_radius), mouse[1]))
+    angular_coefficient = (circle_center[1] - mouse[1]) / ((circle_radius * math.cos((mouse[1] - HEIGHT/2)/circle_radius) - circle_border) - focus)
+
+    pygame.draw.line(screen, (255, 0, 0), (mouse[0], mouse[1]), ((circle_radius * math.cos((mouse[1] - HEIGHT/2)/circle_radius) - circle_border), mouse[1]))
+    pygame.draw.line(screen, (255, 0, 0), ((circle_radius * math.cos((mouse[1] - HEIGHT/2)/circle_radius) - circle_border), mouse[1]), (focus - WIDTH, circle_center[1] + WIDTH*angular_coefficient))
 
     # Towards vertex of mirror
-    pygame.draw.line(screen, (255, 0, 0), mouse, [circle_center[0] + circle_radius, circle_center[1]])
-    pygame.draw.line(screen, (255, 0, 0), [(mouse[0])-50000,
-                                           (circle_center[1] - (mouse[1] - circle_center[1]))+(50000*angular_coefficient)],
-                     [circle_center[0] + circle_radius, circle_center[1]])
+    angular_coefficient = (circle_center[1] - mouse[1]) / (circle_radius - mouse[0])
+
+    pygame.draw.line(screen, (255, 0, 0), mouse, [circle_center[0] + circle_radius - circle_border, circle_center[1]])
+    pygame.draw.line(screen, (255, 0, 0), [(mouse[0])-WIDTH - circle_border,
+                                           (circle_center[1] - (mouse[1] - circle_center[1]))+(WIDTH*angular_coefficient)],
+                     [circle_center[0] + circle_radius - circle_border, circle_center[1]])
 
     pygame.display.update()
 
